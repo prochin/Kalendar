727e3ff4f303985416e0e39dec52565248ce2742
prochin <info@prochin.cz>
2011-12-23 10:42:54 +0100

    first commit

---
+++ src/kalendar/Kalendar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalendar;
+
+/**
+ *
+ * @author prochin
+ */
+public class Kalendar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
